38451567d6620b54d456284c1d77e45db560f7ad
Hesam V. <hesam.vahib@gmail.com>
2025-07-29 17:35:28 +0300

    adding useResource as a Custom Hook to fetch the data from the server

--- 7.1-7.6/src/App.jsx
+++ 7.1-7.6/src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react'
 import {
-  BrowserRouter as Router,
   Route, Routes, Link, useMatch, useNavigate
 } from 'react-router-dom'
+import { useField } from './hooks'
+import { use } from 'react'
+
 
 const Menu = () => {
   const padding = {
@@ -50,24 +52,27 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const {reset: contentReset, ...content} = useField('text')
+  const {reset: authorReset, ...author} = useField('text')
+  const {reset: infoReset, ...info} = useField('text')
+
   const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
-  
-    setContent('')
-    setAuthor('')
-    setInfo('')
     navigate('/')
+  }
+
+  const resetFields = () => {
+    contentReset()
+    authorReset()
+    infoReset()
   }
 
   return (
@@ -76,17 +81,18 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...info} />
         </div>
         <button>create</button>
+      <button type="button" onClick={resetFields}>reset</button>
       </form>
     </div>
   )
